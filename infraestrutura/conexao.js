a936bbab6f4ae77e2ec18a41f756b83005afdc2a
zeleandroxavier <zeleandroxavier@gmail.com>
2021-07-20 21:52:11 -0300

    adicioando parametros no banco de dados

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,11 +1,21 @@
 const mysql = require('mysql');
 
-const conexao = mysql.createConnection ({
+/*const conexao = mysql.createConnection ({
     host: 'localhost',
     port: '3306',
     user: 'root',
     password: 'Jas1584Desc!@',
     database: 'aluraflix-bd'
+});*/
+
+const conexao = mysql.createConnection ({
+    host: 'www.easyweb.dev.br',
+    port: '3306',
+    user: 'easywe34_alurafl',
+    password: '123mudar!@',
+    database: 'easywe34_aluraflix'
 });
+
+
 
 module.exports = conexao;
